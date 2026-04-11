d4a2f3fd145b885cb9384588f9d494819f867347
Nicholas Sokol <nicholassokol@NicksBigMac.local>
2026-04-11 08:35:56 -0400

    Fixes 2.7

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
       "react/no-unescaped-entities": "off",
       "@next/next/no-html-link-for-pages": "off",
       "@next/next/no-img-element": "off",
